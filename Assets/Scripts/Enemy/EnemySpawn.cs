0aa7f498c1bea5490e833b138d65a1d775e427c1
Patrickode <54603257+Patrickode@users.noreply.github.com>
2020-02-01 14:05:27 -0500

    Assigned random player to the spawned enemies' wall field
    Later this can be dynamically assigned and stuff.

--- Assets/Scripts/Enemy/EnemySpawn.cs
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -6,6 +6,7 @@ public class EnemySpawn : MonoBehaviour
 {
     public GameObject baseEnemyPrefab;
     public GameObject playerTargetingEnemyPrefab;
+    public List<GameObject> walls;
 
     /// <summary>
     /// The radius of the circular area the enemies spawn in.
@@ -20,12 +21,20 @@ public class EnemySpawn : MonoBehaviour
 
     void Start()
     {
+        //Initalize the walls list.
+        walls = new List<GameObject>(); 
 
+        //Get all the walls (PLAYERS USED AS PLACEHODLER) and add them to the walls list.
+        Player[] foundObjs = FindObjectsOfType<Player>();
+        foreach (Player obj in foundObjs)
+        {
+            walls.Add(obj.gameObject);
+        }
     }
 
     void Update()
     {
-        SpawnEnemy(baseEnemyPrefab);
+        SpawnEnemy(playerTargetingEnemyPrefab);
     }
 
     private void SpawnEnemy(GameObject enemyPrefab)
@@ -41,8 +50,8 @@ public class EnemySpawn : MonoBehaviour
             float randomY = 0;
             while (randomX == 0 && randomY == 0)
             {
-                randomX = Random.Range(-1, 1);
-                randomY = Random.Range(-1, 1);
+                randomX = Random.Range(-1f, 1f);
+                randomY = Random.Range(-1f, 1f);
             }
             //Now apply those coords to a normalized vector. This'll be the direction they spawn in
             Vector2 spawnDirection = new Vector2(randomX, randomY).normalized;
@@ -50,7 +59,10 @@ public class EnemySpawn : MonoBehaviour
             Vector2 spawnLocation = spawnDirection * spawnAreaRadius;
 
             //Finally, spawn the enemy with the stuff we made.
-            Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
+            GameObject newEnemy = Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
+
+            //Get the enemy script on the enemy, and set its wall reference to a random entry in the walls list.
+            newEnemy.GetComponent<BaseEnemy>().wall = walls[Random.Range(0, walls.Count)];
 
             //Reset the timer.
             spawnTimer = spawnDelay;
