06ae4749d23279c4a10efdbd98a6c9d4e8241747
Breanna Henriquez <50420371+B-Hen@users.noreply.github.com>
2020-02-01 09:13:08 -0500

    BasicEnemy
    Created the basic enemy that will move thowards a target

---
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour
+{
+    //fields
+    public float speed = 50f;
+
+    private Vector2 currentPosition;
+    private Rigidbody2D rb;
+    public GameObject wall;
+    private Vector2 wallPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        wallPosition = new Vector2(wall.transform.position.x, wall.transform.position.y);
+        wallPosition = wallPosition.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    private void FixedUpdate()
+    {
+        MoveEnemy();
+    }
+
+    public void MoveEnemy()
+    {
+        Vector2 move = wallPosition * speed * Time.deltaTime;
+
+        rb.MovePosition(move + currentPosition);
+    }
+
+}
