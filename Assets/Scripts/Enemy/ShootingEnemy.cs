a352b90bdbbb91a5adef397fda8ac10ba5f7067d
Breanna Henriquez (RIT Student) <bh4974@ad.rit.edu>
2020-02-01 12:53:27 -0500

    Update ShootingEnemy.cs

--- Assets/Scripts/Enemy/ShootingEnemy.cs
+++ Assets/Scripts/Enemy/ShootingEnemy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShootingEnemy : PlayerTagertEnemy
+public class ShootingEnemy : PlayerTargetingEnemy
 {
 
     public float shootDistance = 5.5f;
