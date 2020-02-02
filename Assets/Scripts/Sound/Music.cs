730e26cfe5425ecc91cd526d9c7293d3a8f515bf
Jacob Kemp-Schneider (RIT Student) <jmk6152@ad.rit.edu>
2020-02-02 09:38:23 -0500

    added music manager and collider for vacuole

---
+++ Assets/Scripts/Sound/Music.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+
+        if (!(GameObject.FindGameObjectWithTag("music") != this.gameObject))
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
